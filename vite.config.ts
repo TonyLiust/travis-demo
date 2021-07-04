600d9745caa903f5757db1a292f7bbff29449b3d
TonyLiust <1256129@163.com>
2021-07-04 19:36:52 +0800

    update vue.config.js

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,10 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  /**
+   * 在生产中服务时的基本公共路径。
+   * @default '/'
+   */
+  base: process.env.NODE_ENV === 'production' ? '/travis-demo/' : '/'
 })
